421b6b9655814d9b4e0f9ec5e3354d8f380b1c54
yuto inuda <yuto.dog178135@gmail.com>
2023-12-07 00:57:18 +0900

    🐛 言語の修正

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61c3387f7efee505"; }
+/******/ 		__webpack_require__.h = function() { return "6498ba1f956a918e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/layout/CavaniHead.js
+++ src/layout/CavaniHead.js
@@ -10,7 +10,6 @@ const CavaniHead = () => {
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
       <title>犬田悠斗</title>
-      <html lang="ja"></html>
       {/* STYLES */}
       <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
