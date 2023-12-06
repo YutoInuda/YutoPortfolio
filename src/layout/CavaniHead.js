ba9c4b9e141074cd2789f2d3d0d34bb7c1de1137
yuto inuda <yuto.dog178135@gmail.com>
2023-12-07 00:54:08 +0900

    🐛 metaの書き換え

--- src/layout/CavaniHead.js
+++ src/layout/CavaniHead.js
@@ -3,13 +3,14 @@ const CavaniHead = () => {
   return (
     <Head>
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="description" content="Name of your web site" />
-      <meta name="author" content="Marketify" />
+      <meta name="description" content="犬田悠斗" />
+      <meta name="author" content="犬田悠斗" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
       <title>犬田悠斗</title>
+      <html lang="ja"></html>
       {/* STYLES */}
       <link
         href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
